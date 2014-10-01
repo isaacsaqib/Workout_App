64a2ded2368afa124cc63ce42bcdff7fbac9c208
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-09-30 21:37:56 -0400

    responsive nav

--- app/assets/javascripts/abdominals.js
+++ app/assets/javascripts/abdominals.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(".torso").css("position", "absolute").css("top", "100px").css("left", "400px").animate({left: "900px"}, 500).fadeOut();
     $(".bodypart").hide();
     $("#back-view").hide();
-    getAndAppendVideoIframes("abdominals workout", $("#videoContainer"));
+    getAndAppendVideoIframes("abdominals workout", $("#ninja"));
     $("#videoContainer").show();
     $("#return").hide();
 
