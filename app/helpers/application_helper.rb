946f73be5da0dd213e70c2b025fd394e783988f3
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-10-21 19:09:47 -0400

    removing www function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
 
-   before_action :redirect_to_www if Rails.env == "production"
-
-   def redirect_to_www
-     unless /www\.work-me-out\.com/ =~ request.url
-       uri = URI.parse(request.url)
-       uri.host = "www.work-me-out.com"
-       redirect_to uri.to_s
-     end
-   end
-
 end
 
